33fc4ac8b3ed6cac746721230861e92014746424
DTQ732 <2370681859@qq.com>
2022-03-24 21:46:24 +0800

    day01 day02代码提交

--- day01.c
+++ day01.c
@@ -1,5 +1,3 @@
-#if 0 // 如果要运行该文件，把0改为1，并把其他文件的1（如果有），改为0
-
 /*
 * day01.c
 	第一天，经典hello world!
@@ -10,10 +8,18 @@
 */
 /* 以斜杠星号开始，型号斜杠结束的注释，可以跨多行的注释，也可以写在代码中间，但不能写在双引号之间 */
 #include <stdio.h> // stdio.h 标准 输入输出(输入输出input & output简称io) 库头文件，里面声明了C语言标准委员会确定的io操作函数。键盘输入，屏幕输出。
+/*
 int main() // C程序总是以main函数开始，int表示返回给调用者的类型，main总是返回int值
 { // 一个语句块的开始，程序的开始
 	printf("hello world!\n"); // 语句，printf表示将在窗口上输出引号内的内容，语句总是以分号结尾
 	return 0; // return一个函数遇到return会直接结束，并把后面的东西返回给调用者，要与返回类型匹配
 } // 程序结束
+*/
 
-#endif
+// 整体代码
+#include <stdio.h>
+int main() 
+{
+	printf("hello world!\n"); 
+	return 0;
+}
